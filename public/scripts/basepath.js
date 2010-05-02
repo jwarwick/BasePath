dda19b663fc9f11c25627b1fddac1bacd8630c44
John Warwick <jwarwick@gmail.com>
2010-05-02 08:27:53 -0400

    Hiding/showing team divs

--- public/scripts/basepath.js
+++ public/scripts/basepath.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
-	$("#test").button()
+	$("#al_east").button();		
+	$("#al_central").button();
+	$("#al_west").button();
+	$("#nl_east").button();
+	$("#nl_central").button();
+	$("#nl_west").button();
+	
+	$("#al_east").toggle(function() {showClass("al_east", $("#al_east"))}, 
+						function() {hideClass("al_east", $("#al_east"))});
+	$("#al_central").toggle(function() {showClass("al_central")}, 
+						function() {hideClass("al_central")});
+	$("#al_west").toggle(function() {showClass("al_west")}, 
+						function() {hideClass("al_west")});
+						
+	$("#nl_east").toggle(function() {showClass("nl_east")}, 
+						function() {hideClass("nl_east")});
+	$("#nl_central").toggle(function() {showClass("nl_central")}, 
+						function() {hideClass("nl_central")});
+	$("#nl_west").toggle(function() {showClass("nl_west")}, 
+						function() {hideClass("nl_west")});																
 })
+
+function showClass(divClass, buttonSel)
+{
+	$("." + divClass).slideUp();
+	buttonSel.toggleClass('ui-state-active');
+	
+}
+
+function hideClass(divClass, buttonSel)
+{
+	$("." + divClass).slideDown();
+	buttonSel.toggleClass('ui-state-active');
+	
+}
