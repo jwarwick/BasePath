c8f3b7cb408bb9a3dc4df71324d174a6b2509086
John Warwick <jwarwick@gmail.com>
2010-05-03 22:43:48 -0400

    Adding DataMapper support

--- BasePath.rb
+++ BasePath.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'dm-core'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Team
+  include DataMapper::Resource
+  
+  property :id,           Serial
+  property :name,         String
+end
+
+DataMapper.auto_upgrade!
 
 get '/?' do
   haml :index
